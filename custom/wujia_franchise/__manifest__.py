a6abc5c226f92c13472c7f8f51c4d4db0e09cd3b
Huỳnh Ngô Anh Thái <74443669+huynhngoanhthai@users.noreply.github.com>
2026-07-30 14:33:25 +0700

    xông module phân tích kết quả giám sát

--- custom/wujia_franchise/__manifest__.py
+++ custom/wujia_franchise/__manifest__.py
@@ -45,6 +45,7 @@ Hợp nhất 2 model trong cùng 1 module để tránh circular dep
         'views/wujia_franchise_inspection.xml',
         'views/wujia_franchise_inspection_history_views.xml',
         'views/wujia_franchise_inspection_remediation_views.xml',
+        'views/wujia_franchise_inspection_report_views.xml',
         'views/wujia_supervision_schedule_views.xml',
         'views/res_partner_views.xml',
         'views/res_users_views.xml',
