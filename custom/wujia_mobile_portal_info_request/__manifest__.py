ecf19601eec67addf1b252a1757c649095145206
Huỳnh Ngô Anh Thái <74443669+huynhngoanhthai@users.noreply.github.com>
2026-09-18 11:44:44 +0700

    update UI mobile

---
+++ custom/wujia_mobile_portal_info_request/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Wujia Mobile Portal Info Request',
+    'version': '19.0.1.0.0',
+    'category': 'Wujia',
+    'summary': 'Responsive mobile views and kanban cards for Wujia Info Update Requests',
+    'author': 'WujiaTea',
+    'license': 'LGPL-3',
+    'depends': [
+        'wujia_portal_info_request',
+        'wujia_mobile_core',
+    ],
+    'data': [
+        'views/info_request_mobile_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
