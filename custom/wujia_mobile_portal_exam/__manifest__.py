5eec683aded0d89176f3eed3338b5543db324438
Huỳnh Ngô Anh Thái <74443669+huynhngoanhthai@users.noreply.github.com>
2026-09-18 14:07:26 +0700

    feat: add wujia_mobile_portal_exam module with mobile kanban views and tests

---
+++ custom/wujia_mobile_portal_exam/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Wujia Mobile Portal Exam',
+    'version': '19.0.1.0.0',
+    'category': 'Wujia',
+    'summary': 'Responsive mobile views and kanban cards for Wujia Training & Exams',
+    'author': 'WujiaTea',
+    'license': 'LGPL-3',
+    'depends': [
+        'wujia_portal_exam',
+        'wujia_mobile_core',
+    ],
+    'data': [
+        'views/exam_session_mobile_views.xml',
+        'views/exam_registration_mobile_views.xml',
+        'views/exam_course_mobile_views.xml',
+        'views/exam_time_slot_mobile_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
